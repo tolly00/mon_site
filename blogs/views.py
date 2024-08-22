3348124325071c0fb65ce097a92655664085a708
tolly00 <albertosylveus@gmail.com>
2024-08-22 09:00:13 -0400

    views setup2

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Article
+
 
 # Create your views here.
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article_form.html'
+    fields = ['title', 'content']
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = 'article_form.html'
+    fields = ['title', 'content']
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_confirm_delete.html'
+    success_url = reverse_lazy('article_list')
