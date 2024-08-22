6e3d59fc6b0a149c1c12d87d4aa2b195a2a7bf16
tolly00 <albertosylveus@gmail.com>
2024-08-22 09:22:06 -0400

    retriction added

--- blogs/views.py
+++ blogs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Article
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
@@ -13,17 +14,17 @@ class ArticleDetailView(DetailView):
     model = Article
     template_name = 'article_detail.html'
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = 'article_form.html'
     fields = ['title', 'content']
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = Article
     template_name = 'article_form.html'
     fields = ['title', 'content']
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = 'article_confirm_delete.html'
     success_url = reverse_lazy('article_list')
